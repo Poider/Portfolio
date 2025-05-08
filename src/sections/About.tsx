e862bdb78bbd5c7c4c175c8395f9a61146f7d1e0
poider <moadammari20@gmail.com>
2025-05-08 01:23:31 +0100

    about section skeleton added

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -5,10 +5,13 @@ import "../styles/About.css"
 const About = () => {
     return (
             <div className="AboutMain">
-                <div className="AboutText">
-                    <h2>About Me</h2>
-                    <p>
+            <div className="AboutText">
+                <p className="whosMouad">Who's Mouad</p>
+          
+                <h3>Memorable UI Design For Interactive Experiences</h3>
+                    <p className="MainAboutParagraph">
                         I’m a software engineer with four years of hands-on experience, two of which were in a corporate environment.
+                    When a web designer receives a creative brief, an entire journey is laid out in front of them. Research, visual strategy, UX/UI design, web building, animation, and more. Without directions, this workflow can become tangled, inefficient, and yield poor results. This course provides a map on how to navigate the wonderful world of web design.
                     </p>
                    
                 </div>
